b1f6cdd42b8e84e0d79b8ae1e1d4e80990669250
hanks <chloe88@qq.com>
2015-04-21 16:05:50 +0800

    update match building

--- static/js/lottery.js
+++ static/js/lottery.js
@@ -25,7 +25,37 @@
                         $(this).removeClass("hidden");
                     }
                 });
-                genDetail();
+                var resultArr = [];
+                $("#PassType input:checked").each(function(){
+                    var type = $(this).data("type");
+                    console.log(type);
+                   resultArr =  resultArr.concat(genDetail(type));
+
+                });
+                console.log()
+                $("#BetCount").html(resultArr.length);
+                var min = 0;
+                var max = 0;
+                _.each(resultArr, function(item){
+                    var r = 1;
+                    _.each(item, function(subItem){
+
+                        r  = r * subItem.rate;
+                    });
+
+                    if(!min){
+                      min = r;
+                    }
+                    if(r > max){
+                        max = r;
+                    }
+                    if(r < min){
+                        min = r;
+                    }
+
+                });
+                $("#BetAward").html(min + "-----" + max);
+
             }
 
 
@@ -65,16 +95,13 @@
             }
             //console.log(arr);
 
-            doGen(arr, type, tempList);
-            var listArr = [];
-
-            return rateMap;
-
+            var listArr = doGen(arr, type, tempList);
+            return listArr;
 
 
         }
         function swap(arr,index1, index2){
-           // console.log("do swap");
+            // console.log("do swap");
             // console.log(arr);
             var a = arr[index1];
             var b = arr[index2];
@@ -102,23 +129,57 @@
             //console.log(arr);
 
             var tempArr = [];
+            var max = 1;
+            var total = 1;
             for(var i = 0; i < arr.length; ++i){
                 if(arr[i] == 1){
+                    total = list[i].length * total;
                     tempArr.push(list[i]);
                 }
             }
+
+
             var resultArr = [];
 
             _.each(tempArr, function(item, i){
 
                 var subArr = tempArr[i];
-                for(var j = 0; j < subArr.length; j++){
 
-                    resultArr[i * j] = resultArr[i * j] || [];
-                    var arr = resultArr[i * j];
-                    arr[i] = subArr[j];
 
+                if(i != tempArr.length -1){
+
+                    for(var j = 0; j < subArr.length; j++){
+                        var end = total / subArr.length;
+                        for(var k = j * end; k < (j+1) * end; k++){
+                            resultArr[k] = resultArr[k] || [];
+                            resultArr[k][i] = subArr[j];
+                        }
+
+                    }
+                }else{
+                    (function(){
+
+                        var len =  subArr.length; //3
+                        var end = total / len;  // 4
+
+                        for(var k = 0;k < end; k++)
+                        {
+                            for(var j = 0; j < len; j++){
+                                var idx = k * len + j;
+                                resultArr[idx] = resultArr[idx] || [];
+                                resultArr[idx][i] = subArr[j];
+
+                            }
+
+
+                        }
+
+                    })();
                 }
+
+
+
+
 
             });
 
@@ -133,10 +194,11 @@
 
             total = vecInt.length;
             var arr = vecInt;
-            printResult(vecInt, list);
+            var resultArr = [];
+            resultArr.push(printResult(vecInt, list));
             var times = 0;
             var start = 0;
-            var resultArr = [];
+
             for(var  i = 0; i < total - 1; ++i){
                 console.log("before:",JSON.stringify(arr));
                 console.log("i is:",i);
@@ -156,7 +218,7 @@
                     console.log("after sort",JSON.stringify(arr));
 
                     //after step 1 and 2, a new combination is exist
-                   var result =  printResult(vecInt, list);
+                    var result =  printResult(vecInt, list);
                     resultArr.push(result);
                     //try do step 1 and 2 from front
                     i = -1;
@@ -165,11 +227,25 @@
                     vecInt = arr;
                 }
             }
-            console.log("after sort",JSON.stringify(resultArr));
-            _.each(resultArr, function(item){
-                console.log("result is ",JSON.stringify(item));
-            });
-            console.log("times is :" ,times);
+            //console.log("after sort",JSON.stringify(resultArr));
+            var finalResult = [];
+            function show(item){
+                console.log(typeof(item[0]));
+                if(_.isArray(item) && _.isArray(item[0])){
+                    _.each(item, function(item){
+                        show(item);
+                    });
+                }else{
+
+                        console.log("result is ",JSON.stringify(item));
+                    finalResult.push(item);
+                }
+
+            }
+            show(resultArr);
+
+            return finalResult;
+            //console.log("times is :" ,times);
 
         }
 
@@ -209,9 +285,7 @@
 
         $("#PassType input[type='checkbox']").on("click", function(){
 
-            var type = $(this).data("type");
-            console.log(type);
-            genDetail(type);
+            render();
 
         });
 
